0ee23d06069e55c332600705534e7b40b0b175fe
tomas <chanyshevd6@gmail.com>
2026-02-08 11:29:47 +0700

    word task

--- app/src/main/java/com/example/myapplication/presentation/exercise_word_practice/wordPracticetask.kt
+++ app/src/main/java/com/example/myapplication/presentation/exercise_word_practice/wordPracticetask.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight.Companion.W500
@@ -23,12 +27,17 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import com.example.myapplication.presentation.reusable.Space
 import com.example.myapplication.presentation.ui.theme.Blue
+import com.example.myapplication.presentation.ui.theme.Green
 import com.example.myapplication.presentation.ui.theme.LightGray
+import com.example.myapplication.presentation.ui.theme.Orange
 import kotlin.random.Random
 
 
 @Composable
 fun WordsTask(navController: NavController) {
+    var current_answer by remember { mutableStateOf(0) }
+    val true_answer = 1
+    var answered by remember { mutableStateOf(false) }
     Scaffold(
         topBar = { TopBar(navController) }
     ) { innerPadding ->
@@ -54,52 +63,36 @@ fun WordsTask(navController: NavController) {
                 textAlign = TextAlign.Center
             )
             Space(height = 34.dp)
-            Button(
-                {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(LightGray, RoundedCornerShape(12.dp)),
-                contentPadding = PaddingValues(vertical = 16.dp),
-            ) {
-                Text("Муха", color = Color.White, fontSize = 20.sp, fontWeight = W500)
-            }
-            Space(height = 10.dp)
-            Button(
-                {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(LightGray, RoundedCornerShape(12.dp)),
-                contentPadding = PaddingValues(vertical = 16.dp),
-            ) {
-                Text("Садовник", color = Color.White, fontSize = 20.sp, fontWeight = W500)
-            }
-            Space(height = 10.dp)
-            Button(
-                {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(LightGray, RoundedCornerShape(12.dp)),
-                contentPadding = PaddingValues(vertical = 16.dp),
-            ) {
-                Text("Гладиолус", color = Color.White, fontSize = 20.sp, fontWeight = W500)
-            }
-            Space(height = 10.dp)
-            Button(
-                {},
-                colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .background(LightGray, RoundedCornerShape(12.dp)),
-                contentPadding = PaddingValues(vertical = 16.dp),
-            ) {
-                Text("Собака", color = Color.White, fontSize = 20.sp, fontWeight = W500)
+            listOf("Муха", "Садовник", "Гладиолус", "Собака").forEachIndexed { index, string ->
+                var background_color = LightGray
+                if (answered && index == true_answer) {
+                    background_color = Green
+                } else if (answered && index == current_answer) {
+                    background_color = Orange
+                } else if (index == current_answer) {
+                    background_color = Blue
+                }
+                Button(
+                    { current_answer = index },
+                    colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .background(
+                            background_color,
+                            RoundedCornerShape(12.dp)
+                        ),
+                    contentPadding = PaddingValues(vertical = 16.dp),
+                    enabled = !answered
+                ) {
+                    Text(string, color = Color.White, fontSize = 20.sp, fontWeight = W500)
+                }
+                Space(height = 10.dp)
             }
             Spacer(Modifier.weight(1f))
             Button(
-                {},
+                {
+                    answered = true
+                },
                 colors = ButtonDefaults.buttonColors(containerColor = Color.Transparent),
                 modifier = Modifier
                     .fillMaxWidth()
@@ -110,4 +103,8 @@ fun WordsTask(navController: NavController) {
         }
 
     }
+}
+
+fun check_answer(current_answer: Int, true_answer: Int): Boolean {
+    return current_answer == true_answer
 }
